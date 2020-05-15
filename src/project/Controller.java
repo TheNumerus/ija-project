a9b19ed3bc94fa8e1d6ba67d6ca0ac644dd57456
David Rubý <dejfik@icloud.com>
2020-05-15 12:11:03 +0200

    edit mode functionality and comments

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -10,6 +10,7 @@ import javafx.stage.FileChooser;
 import project.gui.MapPane;
 import project.map.*;
 
+import javax.swing.*;
 import java.io.File;
 import java.time.Duration;
 
@@ -21,11 +22,22 @@ public class Controller {
     public Button speedUp_button;
     public Button slowDown_button;
     public Button playPause_button;
+    public Button closures_button;
+    public Button speedAdjustments_button;
+    public Button detours_button;
     public Label speed;
 
     public Map map;
 
     private InternalClock clock;
+
+    enum EditMode{
+        CLOSURES,
+        SPEEDADJUSTMENTS,
+        DETOURS
+    }
+
+    private EditMode currentMode = EditMode.CLOSURES;
 
     public void initialize() {
         clock = new InternalClock(this::tick);
@@ -42,6 +54,7 @@ public class Controller {
         MapPane.renderVehicles(map);
     }
 
+    // sets the environment for new map
     private void loadMap() {
         MapPane.clearMap();
         MapPane.renderMapBase(map);
@@ -50,6 +63,10 @@ public class Controller {
         playPause_button.setDisable(false);
     }
 
+
+    // after Load Data button, file chooser is opened.
+    // loads .json file and loads map
+    // shows popup alert, if the map file is not valid.
     public void loadDataButtonClick(ActionEvent actionEvent) {
         final FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open resource file");
@@ -71,6 +88,11 @@ public class Controller {
         }
     }
 
+
+    /**
+     * calculates hours, minutes and seconds (hh:mm:ss) from Duration.
+     * @param time sets Label to this time
+     */
     public void setTime(Duration time){
         long totalSecs = time.getSeconds();
         String hours = Integer.toString((int)totalSecs / 3600);
@@ -88,6 +110,41 @@ public class Controller {
         this.time.setText("00:00:00");
     }
 
+    // disables clicked button and enables others.
+    // sets current mode to clicked button mode.
+
+    public void closuresButtonClick(ActionEvent actionEvent){
+        this.closures_button.setDisable(true);
+        this.speedAdjustments_button.setDisable(false);
+        this.detours_button.setDisable(false);
+
+        this.currentMode = EditMode.CLOSURES;
+    }
+
+    // disables clicked button and enables others.
+    // sets current mode to clicked button mode.
+    public void speedAdjustmentsButtonClick(ActionEvent actionEvent){
+        this.closures_button.setDisable(false);
+        this.speedAdjustments_button.setDisable(true);
+        this.detours_button.setDisable(false);
+
+        this.currentMode = EditMode.SPEEDADJUSTMENTS;
+    }
+
+    // disables clicked button and enables others.
+    // sets current mode to clicked button mode.
+    public void detoursButtonClick(ActionEvent actionEvent){
+        this.closures_button.setDisable(false);
+        this.speedAdjustments_button.setDisable(false);
+        this.detours_button.setDisable(true);
+
+        this.currentMode = EditMode.DETOURS;
+    }
+
+
+    // speeds up the simulation speed.
+    // disables button, if the simulation speed is on maximum
+    // enables slow down button.
     public void speedUp(ActionEvent actionEvent){
         double currentSpeed = clock.speedUp();
         slowDown_button.setDisable(false);
@@ -97,6 +154,9 @@ public class Controller {
         speed.setText(currentSpeed + "x");
     }
 
+    // slows down the simulation speed.
+    // disables button, if the simulation speed is on minimum.
+    // enables speed up button
     public void slowDown(ActionEvent actionEvent){
         double currentSpeed = clock.speedDown();
         speedUp_button.setDisable(false);
@@ -106,6 +166,8 @@ public class Controller {
         speed.setText(currentSpeed + "x");
     }
 
+    // resets the simulation speed to default.
+    // enables speedUp and slowDown buttons.
     public void resetSpeed(ActionEvent actionEvent){
         clock.resetSpeed();
         speedUp_button.setDisable(false);
@@ -113,6 +175,8 @@ public class Controller {
         speed.setText("1.0x");
     }
 
+    // stops the simulation clock
+    // changes between simulation states
     public void playPause(ActionEvent actionEvent){
         if(clock.isPaused()){
             clock.setPaused(false);
@@ -124,6 +188,8 @@ public class Controller {
         }
     }
 
+
+    // resets the view to default (X and Y axes and scroll)
     public void resetView(ActionEvent actionEvent) {
         MapPane.resetView();
     }
